c7b6582c42051770f66172b512bb11af54fb61d5
JOHN PEREZ QUESADA <john.perez.quesada@est.una.ac.cr>
2024-10-30 15:12:41 -0600

    si

--- ProyectoFiltros/B&W_Filter.h
+++ ProyectoFiltros/B&W_Filter.h
@@ -11,12 +11,12 @@ using namespace std;
 void BnW_Filter(Mat img, Mat modified) {
     for (int y = 0; y < img.rows; y++) {
         for (int x = 0; x < img.cols; x++) {
-         Vec3b color = img.at<Vec3b>(Point(x, y));
-         double graypixel = color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114;
-         color[0] = graypixel;
-         color[1] = graypixel;
-         color[2] = graypixel;
-         modified.at<Vec3b>(Point(x, y)) = color;
+            Vec3b color = img.at<Vec3b>(Point(x, y));
+            double graypixel = color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114;
+            color[0] = graypixel;
+            color[1] = graypixel;
+            color[2] = graypixel;
+            modified.at<Vec3b>(Point(x, y)) = color;
         }
     }
 }
